a2918f9e10e62ea29af430e670a01162e4c05eba
Anas Khaled <anaskhaled23.9.4@gmail.com>
2025-04-29 19:55:34 +0300

    ..

--- landmarks.h
+++ landmarks.h
@@ -10,9 +10,9 @@ typedef struct {
 // List of landmarks with their coordinates ( NAME , Lat , Long )
 Landmark landmarks[NUM_LANDMARKS] = {
 
-    {"HALL A and HALL B"  ,30.06417,31.28},
-    {"HALL C and HALL D" , 30.06361, 31.28028},
-    {" Architecture Building" ,  30.06389,  31.28056},
+    {"HALLS A and B"  ,30.06417,31.28},
+    {"HALLS C and D" , 30.06361, 31.28028},
+    {"Architecture" ,  30.06389,  31.28056},
 
     {"Old Building" , 30.06417, 31.27972},  
     {"Old Building" , 30.06361,  31.27889},           
@@ -22,16 +22,16 @@ Landmark landmarks[NUM_LANDMARKS] = {
     {"Library" , 30.06472, 31.28028},  
     {"Library" , 30.06556,  31.28}, 
 
-    {"Playground " , 30.06417, 31.27972},  
-    {"Playground " ,  30.06361,  31.27889},  
-    {"Playground " ,  30.06333, 31.27917},  
-    {"Playground " , 30.06361,  31.28},  
+    {"Playground" , 30.06417, 31.27972},  
+    {"Playground" ,  30.06361,  31.27889},  
+    {"Playground" ,  30.06333, 31.27917},  
+    {"Playground" , 30.06361,  31.28},  
 
-    {"Luban Workshop Corners" , 30.06333,  31.27972},
-    {"Luban Workshop Corners" , 30.06361, 31.27889},
+    {"Luban Workshop" , 30.06333,  31.27972},
+    {"Luban Workshop" , 30.06361, 31.27889},
 
-    {"Credit Building" , 30.06333,  31.27806},
-    {"Kharasana Building" , 30.06417,  31.2775},
+    {"Credit" , 30.06333,  31.27806},
+    {"Kharasana" , 30.06417,  31.2775},
     {"Mosque" , 30.06472,  31.28028},
    
 };
