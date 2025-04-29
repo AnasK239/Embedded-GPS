389bed0aea4cbdbb225cf0dede3d355a93afc206
Ahmed Samy <ahmedsaso505@gmail.com>
2025-04-29 19:40:00 +0300

    Better comments

--- TIMER/TIMER.h
+++ TIMER/TIMER.h
@@ -8,12 +8,13 @@
 
 #define Max_reload 0x00FFFFFF
 #define Control_shape 0x05
-#define ms_delay 80000
-#define us_delay 80
+#define ms_delay 16000
+#define us_delay 16
 
 
 void SysTick_Init(void);
-void SysTick_Wait(uint32_t delay);
-void SysTick_Wait1ms(uint32_t delay);
+void SysTick_Wait(uint16_t delay);
+void SysTick_Wait1ms(uint16_t delay);
+void SysTick_Wait1us(uint16_t delay);
 
 #endif
